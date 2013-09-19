e88b5cc134980da887e82b44fac01811b315748d
Tobias Rupp <tobias.rupp@gmx.com>
2013-09-19 15:43:52 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tobiasrupp::Application.config.secret_key_base = '56421d882410d82ce48b73ca7cc63a950f1749724021fb73a8d6536a5eb00f7d438ed7c23efd1cad4e831e4f9a7a0e33979c6f9bd379ebf42c0eb76bb3dd7a52'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tobiasrupp::Application.config.session_store :cookie_store, key: '_tobiasrupp_session'
